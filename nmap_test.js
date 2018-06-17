86ca35b7a4c33ccc5f78125fe0ed3a3b14692e3f
Di Liu <unknowngas@gmail.com>
2018-06-17 00:10:40 -0400

    Testing: Parse arp-scan result to IP,MAC

--- nmap_test.js
+++ nmap_test.js
@@ -1,9 +1,10 @@
 var exec = require("child_process").exec;
-dir = exec("ls -la", function(err, stdout, stderr) {
+dir = exec("sudo arp-scan --interface=wlan0 --localnet | sort -V | grep -P '^[0-9]\{1,3\}' | awk -v OFS=\",\" '{ print $1, $2 }'", function(err, stdout, stderr) {
     if (err) {
         // should have err.code here?
     }
     console.log(stdout);
+
 });
 
 dir.on('exit', function (code) {
